461314bfae7ac313cf5bbc63d79f22f004bcb807
Tim Z Liu <995332120@qq.com>
2018-06-03 22:36:59 +0800

    add mongo

--- app/templates/app/service/user-mongo-server.js
+++ app/templates/app/service/user-mongo-server.js
@@ -2,13 +2,10 @@ let userDao = require('../dao/user-dao');
 let validateUser = require('./validate/validate-user');
 
 class userService {
-
     static async getUser(condition) {
         await validateUser.getOneUser(condition);
         return await userDao.find(condition);
     }
-
-
 }
 
 module.exports = userService;
--- app/templates/app/service/validate/validate-mongo-user.js
+++ app/templates/app/service/validate/validate-mongo-user.js
@@ -1,7 +1,15 @@
 class validateUser {
     static async getUser(condition) {
         if (!condition.id) {
-            throw 'User id Lost';
+            throw 'id is Lost';
+        }
+    }
+    static async saveUser(condition) {
+        if (!condition.age) {
+            throw 'age is Lost';
+        }
+        if (!condition.username) {
+            throw 'username is Lost';
         }
     }
 }
