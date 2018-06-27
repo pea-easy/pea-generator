361e247f6019a85e0a8c598be5b5a2fc0e6f21cc
Tim Z Liu <995332120@qq.com>
2018-06-27 11:36:01 +0800

    bug fox

--- app/templates/app/routes/apiguide/user.js
+++ app/templates/app/routes/apiguide/user.js
@@ -2,32 +2,28 @@
 /**
   @apiGroup User
   @apiVersion 0.1.0
-  @api {post} /users/:id   获取用户信息
-  
-  @apiParam {Number} id  1-用户id
-  @apiParamExample {json} Request-Example:
-    {
-      id:1
-    }
- 
+  @api {get} /user/getUser/:id   get user information
+
+  @apiParam {Number} id  1-user id
+
   @apiSuccessExample Success-Response:
-    HTTP/1.1 200 OK 
+    HTTP/1.1 200 OK
   {
     msg:"success",
     "code": 200,
-    
-}  
+
+}
 
 
-  @apiError code   错误代码
-  @apiError msg   错误描述
+  @apiError code   err code
+  @apiError msg   err description
   @apiErrorExample Error-Response:
-      HTTP/1.1 200 
+      HTTP/1.1 200
       {
         code:500,
-        msg: "系统错误."
+        msg: "system."
       }
- 
+
  */
 
 
